f7c3eb8142f7886c64cd868d761e6cb2803a6634
BartoszRajewicz <szejn3@gmail.com>
2023-11-23 19:33:34 +0100

    feat: added js code to 02-video.js

--- src/js/02-video.js
+++ src/js/02-video.js
@@ -1,1 +1,15 @@
+import Player from '@vimeo/player';
+import throttle from 'lodash.throttle';
+const iframe = document.querySelector('#vimeo-player');
+const player = new Player(iframe);
+player.on(
+  'timeupdate',
+  throttle(event => {
+    localStorage.setItem('video-play-time', event.seconds);
+  }, 1000)
+);
 
+window.addEventListener('DOMContentLoaded', () => {
+  const time = localStorage.getItem('video-play-time');
+  player.setCurrentTime(time);
+});
