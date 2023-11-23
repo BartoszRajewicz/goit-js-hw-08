993a7e4054f6a5b7839a77dd1be3c65be81e68db
BartoszRajewicz <szejn3@gmail.com>
2023-11-23 19:53:27 +0100

    feat: added code to 03-feedback.js

--- src/js/03-feedback.js
+++ src/js/03-feedback.js
@@ -1,1 +1,55 @@
+import throttle from 'lodash.throttle';
 
+document.addEventListener('DOMContentLoaded', () => {
+  const form = document.querySelector('.feedback-form');
+
+  form.addEventListener('input', throttle(saveInput, 500));
+
+  window.addEventListener('load', storageDataInput);
+
+  form.addEventListener('submit', handleSubmitForm);
+
+  function saveInput() {
+    const formData = {
+      email: form.email.value,
+      message: form.message.value,
+    };
+    localStorage.setItem('feedback-form-state', JSON.stringify(formData));
+  }
+
+  function storageDataInput() {
+    const storedData = localStorage.getItem('feedback-form-state');
+    if (storedData) {
+      const { email, message } = JSON.parse(storedData);
+
+      form.email.value = email;
+      form.message.value = message;
+    }
+  }
+
+  function handleSubmitForm(event) {
+    event.preventDefault();
+
+    const emailValue = form.email.value;
+    const messageValue = form.message.value;
+
+    localStorage.removeItem('feedback-form-state');
+
+    form.reset();
+
+    console.log('Formularz wysłany', {
+      email: emailValue,
+      message: messageValue,
+    });
+
+    const storedData = localStorage.getItem('feedback-form-state');
+    if (storedData) {
+      const { email, message } = JSON.parse(storedData);
+
+      console.log('Wcześniej zapisane dane', {
+        email: email,
+        message: message,
+      });
+    }
+  }
+});
