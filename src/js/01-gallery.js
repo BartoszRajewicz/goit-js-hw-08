fb4ebde440619e3647453274f3e31961ee272a26
BartoszRajewicz <szejn3@gmail.com>
2023-11-23 19:06:24 +0100

    feat: added js code to gallery.js

--- src/js/01-gallery.js
+++ src/js/01-gallery.js
@@ -1,5 +1,29 @@
 // Add imports above this line
+import SimpleLightbox from 'simplelightbox';
+import 'simplelightbox/dist/simple-lightbox.min.css';
+
 import { galleryItems } from './gallery-items';
 // Change code below this line
 
-console.log(galleryItems);
+const galleryContainer = document.querySelector('ul.gallery');
+
+const images = galleryItems.map(item => {
+  return `
+    <li class ="gallery__item">
+      <a class="gallery__link" href="${item.original}">
+                 <img
+                    class="gallery__image"
+                    src="${item.preview}"
+                    data-source="${item.original}"
+                    alt="${item.description}"
+                />
+             </a>
+    </li>`;
+});
+
+galleryContainer.insertAdjacentHTML('beforeend', images.join(''));
+
+const lightbox = new SimpleLightbox('.gallery a', {
+  captionsData: 'alt',
+  captionDelay: 250,
+});
